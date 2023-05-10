4a6ab8aa42ec628a1cb4c913a1bc10d22508e4d0
Viktoriya Sadlovskaya <v1kusen04ek@mail.ru>
2023-05-10 18:14:48 +0300

    Add l and a comand

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-"""
-Задача №1
-Необходимо реализовать пользовательские команды, которые будут выполнять следующие функции:
-
-s – shelf – команда, которая спросит номер документа и выведет номер полки, на которой он находится;
-Правильно обработайте ситуации, когда пользователь будет вводить несуществующий документ.
-l– list – команда, которая выведет список всех документов в формате passport "2207 876234" "Василий Гупкин";
-a – add – команда, которая добавит новый документ в каталог и в перечень полок, спросив его номер, тип, имя владельца и номер полки, на котором он будет храниться. Корректно обработайте ситуацию, когда пользователь будет пытаться добавить документ на несуществующую полку.
-Внимание: p, s, l, a - это пользовательские команды, а не названия функций. Функции должны иметь выразительное название, передающие её действие.
-
-"""
 documents = [
     {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
     {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
@@ -21,11 +10,14 @@ directories = {
     '3': []
     }
 
-def people(document, number):
-    for folder in document:
-        if folder["number"] == number:
-            print(f'Имя владельца документа: {folder["name"]}')
-        else:
+def people(documents, number):
+    count = 0
+    for folder in documents:
+        count += 1
+        if len(documents) < count:
+            if folder["number"] == number:
+                print(f'Имя владельца документа: {folder["name"]}')
+        else: 
             print('Такого документа нет')
 
 def shelf(directories, number):
@@ -35,20 +27,41 @@ def shelf(directories, number):
     else:
         print("Такого документа нет ни на одной полке")
 
-def list():
+def list(documents):
+    for document in documents:
+        print(document.get("type"), document.get("number"), document.get("name"))
 
+def add(documents, directories):
+    type = input("Уточните тип документа: ")
+    number = input("Уточните номер документа: ")
+    name = input("Уточните владельца документа: ")
+    direct = input("Уточните номер полки, на которую нужно положить документ: ")
+    for directory in directories:
+        count = 0
+        if len(directories) > count:
+            count += 1
+            if direct == directory:
+                dict = {"type": type, "number": number, "name": name}
+                documents.append(dict)
+                directories[direct] = number 
+        else:
+            print("Такой полки не существует!!!")
 
-
-# def program():
-#     input(f"Введите команду p / s / l / a : {comand}") 
-#     document = ''
-#     comand = ''
-#     if comand == 'p':
-#         input(f"Введите номер документа: {document}")
-#         people(documents, document)
-#     elif comand == 's':
-
-#     elif comand == 'l':
-
-#     elif comand == 'a':
-
+def program():
+    while 1 == 1:
+        document = ''
+        comand = str(input("Введите команду p / s / l / a : "))
+        print(comand)
+        if comand == 'p':
+            document = input("Введите номер документа: ")
+            people(documents, document)
+        elif comand == 's':
+            number = input("Введите номер документа: ")
+            shelf(directories, number)
+        elif comand == 'l':
+            list(documents)
+        elif comand == 'a':
+            add(documents, directories)
+        else:
+            print("Нет такой команды")
+program()
